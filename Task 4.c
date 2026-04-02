aa6be51f4a1f530b3fac5f813e473b53a948b8b0
Tural Cavadov <tural.javadzade@icloud.com>
2026-04-02 12:38:09 +0400

    Add program to calculate the cube of a number

---
+++ Task 4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int x, kub;
+    printf("Ededi daxil edin: ");
+    scanf("%d", &x);
+    kub = x * x * x;
+    printf("Ededin kubu: %d\n", kub);
+    return 0;
+}
